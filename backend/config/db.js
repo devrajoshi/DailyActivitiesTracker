b1444219dd56101c7fa826bd5e99fae0563be323
Devraj Joshi <devrajoshi444@gmail.com>
2025-03-27 01:24:35 +0545

    pushed auto generated .js and .ts files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection failed");
-        process.exit(1);
-    }
+  const MONGO_URI = process.env.MONGO_URI;
+
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Exit the process with failure
+  }
 };
 
 export default connectDB;
