e7db22cfbdc77270cf0934b1ae436bd6886c5d63
Devraj Joshi <devrajoshi444@gmail.com>
2025-04-05 04:55:53 +0545

    updated authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import refreshAccessToken from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js"; // Middleware to protect routes
 
 const router = express.Router();
 
-// Refresh token route
-router.post("/refresh-token", refreshAccessToken);
+router.route("/refresh-token").post(protect, refreshAccessToken); // Refresh authentication token
 
 export default router;
